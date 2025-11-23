a3c1d54c7606da87873cbd240e7ca653728394fd
Stan Carver II <stan@a1webconsulting.com>
2025-11-23 05:57:11 -0600

    added inline HTML template

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
 
 get '/' do
-  'Hello World'
+  erb :index
 end
+
+__END__
+
+@@ layout
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Sinatra sings</title>
+  </head>
+  <body><%= yield %></body>
+</html>
+
+@@ index
+<h1>Hello World</h1>
